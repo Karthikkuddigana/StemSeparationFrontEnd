69f82a0cde267ba8f23d3ed814b200deb459db8a
Karthikkuddigana <karthikkuddigana@gmail.com>
2025-06-23 22:48:49 +0530

    Added a basic navbar and sample route

---
+++ app/user/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function UserPage1() {
+  return (
+    <div>User Page 1</div>
+  )
+}
---
+++ components/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='p-5 bg-amber-300 flex justify-center font-bold border-5 mb-5'>Spleeter Frontend</div>
+  )
+}
